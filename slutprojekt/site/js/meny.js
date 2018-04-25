bf24db38f0b5c85b7ee6a13d229047be5f7986f7
itggot-karl-ny <35599808+itggot-karl-ny@users.noreply.github.com>
2018-04-25 16:47:39 +0200

    animerad meny

--- slutprojekt/site/js/meny.js
+++ slutprojekt/site/js/meny.js
@@ -2,12 +2,9 @@ function toggleMenu(){
     var element = document.querySelector(".bar");
     element.classList.toggle("show");
 
-    element = document.querySelector("main");
-    element.classList.toggle("fade")
 
     element = document.querySelector(".bar");
-    element.classList.toggle("animation")
+    element.classList.toggle("animation");
 
-    
 }
 
