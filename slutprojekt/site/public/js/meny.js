793f761d5c70175f14876c47878e7ec8a2f7e3a9
itggot-karl-ny <35599808+itggot-karl-ny@users.noreply.github.com>
2018-04-27 13:59:20 +0200

    konverterat till stasis och lagt element i main

---
+++ slutprojekt/site/public/js/meny.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    var element = document.querySelector(".bar");
+    element.classList.toggle("show");
+
+
+    element = document.querySelector(".bar");
+    element.classList.toggle("animation");
+
+}
+
